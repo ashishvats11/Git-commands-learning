93ca48b4a71ecef82c90d1bf11a7893373297e92
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2026-02-24 17:29:20 +0530

    Variables completed

--- python_practice.py
+++ python_practice.py
@@ -1,16 +1,21 @@
 #basic print statement and I am the best coder in the world
 print("Hello World")
 print('''Hello world1, I am a deadly virus currently under deverlopment but once I am ready i will make you all cry.
-      I will be worse than the AI Apocalypse''')     #this is paragraph writing in python, in this i can write in multiple lines
+      I will be worse than the AI Apocalypse!! hahahaha!''')     #this is paragraph writing in python, in this i can write in multiple lines
 
 #variables - used to store values
 name = "Ashish"
 age = 23
 salary = 9999.99
-print(f"My name is {name} and I am {age} years old. Currently I am the president of my own world and my current salary is {salary}")
+print(f"My name is {name} and I am {age} years old. Currently I am the president of my own world and my current salary is {salary}") # this is formatted string format
 
 #switch values of two variables
 v1 = 5
 v2 = 10
-v1,v2 = v2,v1
 print(f"v1 value is {v1} and v2 value is {v2}")
+v1,v2 = v2,v1
+print(f"v1 value is {v1} and v2 value is {v2} after swap")
+
+# Multiple variable assignment
+a,b,c = 1,2,4
+print(f"These are the variables values for a,b and c {a}, {b}, {c} respectively")
