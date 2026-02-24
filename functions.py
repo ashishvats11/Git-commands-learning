d4ae43574969231a451d41f64872b1a2527829c8
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2026-02-24 21:21:20 +0530

    function definition with and without parameters

--- functions.py
+++ functions.py
@@ -1,1 +1,14 @@
 # in this we will be learning about python functions
+
+#define function - def is the keyword to define function
+
+def function():
+    print("Hello, welcome to your first function")
+
+#invoking/calling the function
+function() # this will call the function and will print the statement inside the function
+
+#function with parameters 
+def student(name,age,grade):
+    return f'I am {name} and I am {age} years old and Study in class {grade}'
+print(student('Ashish',23,'2nd')) # in here we had to print the function because we were returning the result instead of printing inside the function
