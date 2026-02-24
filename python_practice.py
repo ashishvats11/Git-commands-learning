c987fbadd23dcdd2417b94284edecac738d7cb1b
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2026-02-24 17:18:06 +0530

    Variables defined

--- python_practice.py
+++ python_practice.py
@@ -1,4 +1,16 @@
 #basic print statement and I am the best coder in the world
 print("Hello World")
 print('''Hello world1, I am a deadly virus currently under deverlopment but once I am ready i will make you all cry.
-      I will be worse than the AI Apocalypse''')     
+      I will be worse than the AI Apocalypse''')     #this is paragraph writing in python, in this i can write in multiple lines
+
+#variables - used to store values
+name = "Ashish"
+age = 23
+salary = 9999.99
+print(f"My name is {name} and I am {age} years old. Currently I am the president of my own world and my current salary is {salary}")
+
+#switch values of two variables
+v1 = 5
+v2 = 10
+v1,v2 = v2,v1
+print(f"v1 value is {v1} and v2 value is {v2}")
