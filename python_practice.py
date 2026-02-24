8387c048146ee347151f439ad069992a6874d4fd
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2026-02-24 15:30:12 +0530

    Added the basic print statement

--- python_practice.py
+++ python_practice.py
@@ -1,1 +1,4 @@
+#basic print statement and I am the best coder in the world
 print("Hello World")
+print('''Hello world1, I am a deadly virus currently under deverlopment but once I am ready i will make you all cry.
+      I will be worse than the AI Apocalypse''')     
