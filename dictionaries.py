321eb88b4c81ea38d4ed998c5f1d2976f4f8a88d
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2026-02-28 04:09:58 +0530

    accessing the dictionary elements

--- dictionaries.py
+++ dictionaries.py
@@ -25,7 +25,7 @@ print("This is an empty dictionary",empty_dict)
 # 2 From an iterable of key-value pairs: Pass an iterable 
 # (like a list or a tuple) containing two-item tuples, where the first item in each tuple is the key and the second is the value.
 
-pairs = [('name','Bob'),('age',23)] # you can  only pass two values as key value pairs in each () or []
+pairs = [('name','Bob'),('age',23)] # you can  only pass two values as key-value pairs in each () or []
 person_dict = dict(pairs)
 print(f'Dictionary created from iterabble {person_dict}')
 
@@ -36,3 +36,35 @@ print(test1)
 #3 third way of creating dictionary
 p1 = dict(name = 'Ashish',age = 23,salary = 10200201200, country ='India')
 print(p1)
+
+# Accessing the dictionary items
+cars = dict(brand = 'Audi', year = 2909, model = 'qss69', designed_by = 'Ashish')
+
+# by passing the key name
+print(f'Accessing the dictionary element using the key name {cars["brand"]}')
+
+# by using the .get() method
+print(f'Accessing the dictionary item using .get(): {cars.get('brand')}')
+
+# .keys() function - return the list of keys in the dictionary
+print(f'Using the .keys() we get all the keys in the dictionary: {cars.keys()}')
+
+# .values() - return the list of all the values in the dictionary
+print(f'Using the .values() we get all the values in the dictionary: {cars.values()}')
+
+# Update the value of a key
+cars['model'] = 'Q8'
+print(f'Dictionary after updating one of the key\'s value {cars}')
+
+# Adding new item to the dictionary
+cars['colour'] = 'Black'
+print('Updated dictionary cars with color field added ',cars)
+
+# items() - This method will give each list of items in the dictionary as tuples in a list [(key,value), (key, value)]
+print(f'These are the items in the dictionary {cars.items()}')
+
+# Check if key exists in the dictionary
+if 'year' in cars:  # this will only work for the keys not the values
+    print('This key is present')
+else:
+    print("Sorry, this key is not present")
