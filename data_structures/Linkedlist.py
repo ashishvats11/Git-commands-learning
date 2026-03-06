2de56cba97c999fc03d2f67009e824e9fd8dbe17
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2026-03-07 02:20:11 +0530

    Created a Linked list and few insertion methods

---
+++ data_structures/Linkedlist.py
@@ -0,0 +1,70 @@
+# In this we will be learning about the Linkedlist
+
+# Creating a class for creating a node in python
+class Node:
+    def __init__(self, data = None, next = None):           
+        self.data = data 
+        self.next = next
+
+class singlyLinkedList:
+    def __init__(self,head = None):
+        self.head = head
+    
+    # add node at the end 
+    def insertAtEnd(self,value):
+        node = Node(value)
+        
+        if self.head != None:
+            curr_node = self.head
+            while curr_node.next !=None:
+                curr_node = curr_node.next
+            curr_node.next = node
+            node.next = None
+        else:
+            self.head = node
+
+    # add node at the begining
+    def insertAtBegining(self, value):
+        node = Node(value)
+        if self.head != None:
+            node.next = self.head
+            self.head = node
+        else:
+            self.head = node
+    
+    # add node in between - add after the value defined
+    def insertInBetween(self, value, x):
+        node = Node(value)
+        if self.head!=None:
+            curr_node = self.head
+            while(curr_node.data != x and curr_node!=None):
+                curr_node = curr_node.next
+            node.next = curr_node.next
+            curr_node.next = node
+            
+        else:
+            self.head = node
+        return
+
+    
+    def print_linkedlist (self):
+        curr_node = self.head
+        while (curr_node.next != None):
+            print(curr_node.data, end = ' ---> ')
+            curr_node = curr_node.next
+        print(curr_node.data) 
+
+  
+
+
+
+LL_1 = singlyLinkedList()
+LL_1.insertAtEnd(10)
+LL_1.insertAtEnd(20)
+LL_1.insertAtEnd(30)
+LL_1.insertAtEnd(40)
+LL_1.insertAtBegining(100)
+LL_1.insertInBetween(50,20)
+LL_1.print_linkedlist()
+
+ 
