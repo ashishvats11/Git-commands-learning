f8b122f2e831ecfc382084027aa442ff77332ae9
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2026-03-21 17:05:35 +0530

    Added the Selection Sort logic

--- sorting_algo.py
+++ sorting_algo.py
@@ -15,7 +15,22 @@ def bubbleSort(array):
             break
     return array
 
-print(bubbleSort(a))
-b = [8,4,3,1,6,2,0]
-print(bubbleSort(b))
+print(f'Bubble Sort - {bubbleSort(a)}')
+b = [8,4,3,1,6,2,0,1]
+print(f'Bubble Sort {bubbleSort(b)}')
 
+# 2. Selection Sort - In this we find the smallest number and bring it to the starting of the array(by swapping)
+def selectionSort(array):
+    n = len(array)
+    for i in range(n):
+        min_value = array[i]
+        min_index = i
+        for j in range(i+1, n):
+            if array[j] < min_value:
+                min_value = array[j]
+                min_index = j  
+        array[i],array[min_index] = array[min_index],array[i]
+      
+    return array
+
+print(f'Selection Sort {selectionSort(b)}')
